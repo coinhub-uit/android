d23db746a7dc3c762a86060d91b5d9fdb5348271
NTGNguyen <ntgnguyen@duck.com>
2025-05-29 13:35:57 +0700

    feat: splash screen

--- app/src/main/java/com/coinhub/android/MainActivity.kt
+++ app/src/main/java/com/coinhub/android/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.coinhub.android.data.remote.SupabaseService
 import com.coinhub.android.presentation.navigation.NavGraph
 import com.coinhub.android.ui.theme.CoinhubTheme
@@ -31,6 +32,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        installSplashScreen()
         setContent {
             LaunchedEffect(Unit) {
                 isSignedIn = supabaseService.isUserSignedIn()
