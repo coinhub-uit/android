1f62f2d1f9271074b9e65779c6c569be2da728df
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-02 19:10:09 +0700

    chore(confirm_auth): card it

--- app/src/main/java/com/coinhub/android/presentation/confirm_auth/ConfirmAuthScreen.kt
+++ app/src/main/java/com/coinhub/android/presentation/confirm_auth/ConfirmAuthScreen.kt
@@ -2,7 +2,9 @@ package com.coinhub.android.presentation.confirm_auth
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,25 +20,39 @@ import com.coinhub.android.utils.PreviewDeviceSpecs
 @Composable
 fun ConfirmAccountScreen() {
     Column(
-        modifier = Modifier.padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
+        modifier = Modifier
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "Confirm Your Account",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(bottom = 32.dp)
-        )
-        Text(
-            text = "We've sent you an email.\nPlease check your inbox and click the confirmation link to activate your account.",
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
-        Text(
-            text = "⚠️ Make sure to open the link on the same phone to be redirected properly.",
-            color = MaterialTheme.colorScheme.error,
-            style = MaterialTheme.typography.bodySmall,
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(24.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Confirm Your Account",
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(bottom = 32.dp)
+                )
+                Text(
+                    text = "We've sent you an email.\nPlease check your inbox and click the confirmation link to activate your account.",
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+                Text(
+                    text = "⚠️ Make sure to open the link on the same phone to be redirected properly.",
+                    color = MaterialTheme.colorScheme.error,
+                    style = MaterialTheme.typography.bodySmall,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
+            }
+        }
     }
 }
 
