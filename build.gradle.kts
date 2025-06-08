f19e12f1245f545e70ca73884722862b253d0cdc
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-08 19:03:21 +0700

    ci: setup sonar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    id("org.sonarqube") version "6.2.0.5505"
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.hilt) apply false
     alias(libs.plugins.google.ksp) apply false
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "coinhub-uit_android")
+        property("sonar.organization", "coinhub-uit")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
