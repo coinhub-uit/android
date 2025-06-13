40361f796da7013a6cd6c2e130bd2ab5142ec0b3
NTGNguyen <ntgnguyen@duck.com>
2025-06-13 20:49:39 +0700

    fix: ticket, yay

--- app/src/main/java/com/coinhub/android/data/dtos/request/TicketRequestDto.kt
+++ app/src/main/java/com/coinhub/android/data/dtos/request/TicketRequestDto.kt
@@ -2,7 +2,7 @@ package com.coinhub.android.data.dtos.request
 
 data class CreateTicketRequestDto(
     val sourceId: String,
-    val methodEnum: String,
+    val method: String,
     val planHistoryId: Int,
     val amount: Long,
 )
--- app/src/main/java/com/coinhub/android/domain/use_cases/CreateTicketUseCase.kt
+++ app/src/main/java/com/coinhub/android/domain/use_cases/CreateTicketUseCase.kt
@@ -1,10 +1,8 @@
 package com.coinhub.android.domain.use_cases
 
-import com.coinhub.android.common.toTicketModel
 import com.coinhub.android.data.api_services.TicketApiService
 import com.coinhub.android.data.dtos.request.CreateTicketRequestDto
 import com.coinhub.android.di.IoDispatcher
-import com.coinhub.android.domain.models.TicketModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -16,7 +14,8 @@ class CreateTicketUseCase @Inject constructor(
     suspend operator fun invoke(createTicketRequestDto: CreateTicketRequestDto): Result {
         return withContext(ioDispatcher) {
             try {
-                Result.Success(ticketApiService.createTicket(createTicketRequestDto).toTicketModel())
+                ticketApiService.createTicket(createTicketRequestDto)
+                Result.Success
             } catch (e: Exception) {
                 Result.Error(e.message ?: "An error occurred while creating the ticket")
             }
@@ -24,7 +23,7 @@ class CreateTicketUseCase @Inject constructor(
     }
 
     sealed class Result {
-        data class Success(val ticketModel: TicketModel) : Result()
+        data object Success : Result()
         data class Error(val message: String) : Result()
     }
 }
