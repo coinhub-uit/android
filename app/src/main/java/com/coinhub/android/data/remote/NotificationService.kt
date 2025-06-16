6151b9b3ab8d9a28c891ce528d531247bde3e543
NTGNguyen <ntgnguyen@duck.com>
2025-06-16 16:48:12 +0700

    feat: show notification

--- app/src/main/java/com/coinhub/android/data/remote/NotificationService.kt
+++ app/src/main/java/com/coinhub/android/data/remote/NotificationService.kt
@@ -1,6 +1,13 @@
 package com.coinhub.android.data.remote
 
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.util.Log
+import com.coinhub.android.MainActivity
+import com.coinhub.android.R
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -13,13 +20,26 @@ class NotificationService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
         if (remoteMessage.notification != null) {
-            Log.d(
-                "dgauiwdghwankjd", "${
-                    remoteMessage
-                        .notification
-                }"
-            )
-            //TODO:Handle the notification here, later
+            showNotification(remoteMessage.notification?.title!!, remoteMessage.notification?.body!!)
         }
+    }
+
+    private fun showNotification(title: String, body: String) {
+        val builder: Notification.Builder? = Notification.Builder(this, "")
+            .setSmallIcon(R.drawable.coinhub)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        notificationIntent.putExtra("message", body)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0, notificationIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        builder?.setContentIntent(pendingIntent)
+        val manager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.notify(0, builder?.build())
     }
 }
