ca34cf2631a570e6083b7c020358a785efb7fc24
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-06-18 00:30:53 +0700

    refactor(menu): move avatar to common components

---
+++ app/src/main/java/com/coinhub/android/presentation/common/components/Avatar.kt
@@ -0,0 +1,87 @@
+package com.coinhub.android.presentation.common.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.SubcomposeAsyncImage
+
+@Composable
+fun Avatar(
+    modifier: Modifier = Modifier,
+    onClick: (() -> Unit)?,
+    url: String,
+    fullName: String? = null,
+) {
+    SubcomposeAsyncImage(
+        model = url,
+        contentDescription = if (fullName != null) "Avatar for $fullName" else null,
+        contentScale = ContentScale.Crop,
+        loading = {
+            CircularProgressIndicator(
+                modifier = Modifier.size(24.dp),
+                color = MaterialTheme.colorScheme.onPrimary,
+            )
+        },
+        modifier = modifier
+            .clip(CircleShape)
+            .border(
+                width = 3.dp, color = MaterialTheme.colorScheme.primary, shape = CircleShape
+            )
+            .let {
+                if (onClick != null) {
+                    it.clickable { onClick() }
+                } else {
+                    it
+                }
+            }
+    )
+}
+
+@Composable
+fun Avatar(
+    modifier: Modifier = Modifier,
+    onClick: (() -> Unit)?,
+    fullName: String,
+) {
+    val initial = fullName.first().toString()
+
+    BoxWithConstraints(
+        modifier = modifier
+            .clip(CircleShape)
+            .background(MaterialTheme.colorScheme.onSecondary)
+            .border(
+                width = 3.dp, color = MaterialTheme.colorScheme.secondary, shape = CircleShape
+            )
+            .let {
+                if (onClick != null) {
+                    it.clickable { onClick() }
+                } else {
+                    it
+                }
+            },
+        contentAlignment = Alignment.Center,
+    ) {
+        val fontSize = (minOf(maxWidth, maxHeight).value * 0.5f).sp
+
+        Text(
+            text = initial,
+            fontWeight = FontWeight.Bold,
+            fontSize = fontSize,
+            maxLines = 1
+        )
+    }
+}
--- app/src/main/java/com/coinhub/android/presentation/menu/components/MenuAvatar.kt
+++ app/src/main/java/com/coinhub/android/presentation/menu/components/MenuAvatar.kt
@@ -1,28 +1,19 @@
 package com.coinhub.android.presentation.menu.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.PreviewLightDark
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import coil.compose.SubcomposeAsyncImage
 import com.coinhub.android.domain.models.UserModel
+import com.coinhub.android.presentation.common.components.Avatar
 import com.coinhub.android.ui.theme.CoinhubTheme
 import java.time.LocalDate
 import java.time.ZonedDateTime
@@ -33,76 +24,33 @@ import kotlin.uuid.Uuid
 fun MenuAvatar(
     user: UserModel?,
 ) {
-    val avatarSize = 128.dp
+    val modifier = Modifier.size(128.dp)
     Column(
         horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
     ) {
         if (user?.avatar != null) {
-            AvatarImage(
-                avatarUrl = user.avatar,
+            Avatar(
+                modifier = modifier,
+                onClick = null,
+                url = user.avatar,
                 fullName = user.fullName,
-                avatarSize = avatarSize,
             )
-        } else {
-            InitialAvatar(
-                fullName = user?.fullName ?: "...",
-                avatarSize = avatarSize,
+        } else if (user != null) {
+            Avatar(
+                modifier = modifier,
+                onClick = null,
+                fullName = user.fullName,
             )
         }
+
         Text(
             text = user?.fullName ?: "...",
             style = MaterialTheme.typography.titleMedium,
             color = MaterialTheme.colorScheme.onSurface,
             softWrap = true,
             modifier = Modifier
-                .padding(top = 4.dp)
+                .padding(top = 8.dp)
                 .align(Alignment.CenterHorizontally)
-        )
-    }
-}
-
-@Composable
-private fun AvatarImage(
-    avatarUrl: String,
-    fullName: String,
-    avatarSize: Dp,
-) {
-    SubcomposeAsyncImage(
-        model = avatarUrl, contentDescription = "Avatar for $fullName", contentScale = ContentScale.Crop, loading = {
-            CircularProgressIndicator(
-                modifier = Modifier.size(24.dp),
-                color = MaterialTheme.colorScheme.onPrimary,
-            )
-        }, modifier = Modifier
-            .size(avatarSize)
-            .clip(CircleShape)
-            .border(
-                width = 3.dp, color = MaterialTheme.colorScheme.primary, shape = CircleShape
-            )
-    )
-}
-
-@Composable
-private fun InitialAvatar(
-    fullName: String,
-    avatarSize: Dp,
-) {
-    val initial = fullName.first().toString()
-
-    Box(
-        modifier = Modifier
-            .size(avatarSize)
-            .clip(CircleShape)
-            .background(MaterialTheme.colorScheme.onSecondary)
-            .border(
-                width = 3.dp, color = MaterialTheme.colorScheme.secondary, shape = CircleShape
-            ), contentAlignment = Alignment.Center
-    ) {
-        Text(
-            text = initial,
-            color = Color.White,
-            fontWeight = FontWeight.Bold,
-            style = MaterialTheme.typography.titleMedium
         )
     }
 }
@@ -112,18 +60,20 @@ private fun InitialAvatar(
 @Composable
 fun MenuAvatarPreview() {
     CoinhubTheme {
-        MenuAvatar(
-            user = UserModel(
-                id = Uuid.random(),
-                birthDate = LocalDate.parse("2000-01-01"),
-                citizenId = "1234567890123",
-                createdAt = ZonedDateTime.parse("2023-01-01"),
-                deletedAt = null,
-                avatar = "https://avatars.githubusercontent.com/u/86353526?v=4",
-                fullName = "NTGNguyen",
-                address = null
-            ),
-        )
+        Surface {
+            MenuAvatar(
+                user = UserModel(
+                    id = Uuid.random(),
+                    birthDate = LocalDate.parse("2000-01-01"),
+                    citizenId = "1234567890123",
+                    createdAt = ZonedDateTime.parse("2023-01-01T00:00:00Z"),
+                    deletedAt = null,
+                    avatar = "https://avatars.githubusercontent.com/u/86353526?v=4",
+                    fullName = "NTGNguyen",
+                    address = null
+                ),
+            )
+        }
     }
 }
 
@@ -132,17 +82,19 @@ fun MenuAvatarPreview() {
 @Composable
 fun MenuAvatarNoImagePreview() {
     CoinhubTheme {
-        MenuAvatar(
-            user = UserModel(
-                id = Uuid.random(),
-                birthDate = LocalDate.parse("2000-01-01"),
-                citizenId = "1234567890123",
-                createdAt = ZonedDateTime.parse("2023-01-01"),
-                deletedAt = null,
-                avatar = null,
-                fullName = "NTGNguyen",
-                address = null
-            ),
-        )
+        Surface {
+            MenuAvatar(
+                user = UserModel(
+                    id = Uuid.random(),
+                    birthDate = LocalDate.parse("2000-01-01"),
+                    citizenId = "1234567890123",
+                    createdAt = ZonedDateTime.parse("2023-01-01T00:00:00Z"),
+                    deletedAt = null,
+                    avatar = null,
+                    fullName = "NTGNguyen",
+                    address = null
+                ),
+            )
+        }
     }
 }
