685e7948d05a353b77d358b3d6aa043717baab16
NTGNguyen <ntgnguyen@duck.com>
2025-06-27 17:03:07 +0700

    fix: it should be subclass

--- app/src/main/java/com/coinhub/android/data/repositories/SettingRepositoryImpl.kt
+++ app/src/main/java/com/coinhub/android/data/repositories/SettingRepositoryImpl.kt
@@ -3,12 +3,13 @@ package com.coinhub.android.data.repositories
 import com.coinhub.android.common.toSettingsModel
 import com.coinhub.android.data.api_services.SettingApiService
 import com.coinhub.android.domain.models.SettingModel
+import com.coinhub.android.domain.repositories.SettingRepository
 import javax.inject.Inject
 
 class SettingRepositoryImpl @Inject constructor(
     private val settingsApiService: SettingApiService,
-) {
-    suspend fun getSettings(): SettingModel? {
+) : SettingRepository {
+    override suspend fun getSettings(): SettingModel? {
         return try {
             settingsApiService.getSettings().toSettingsModel()
         } catch (e: Exception) {
