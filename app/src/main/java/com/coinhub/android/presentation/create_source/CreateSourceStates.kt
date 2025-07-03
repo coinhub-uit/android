39947c4d7d9452be89d349030a1868fa6006df00
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-07-03 20:19:30 +0700

    chore(create_source): not valid for the first time

--- app/src/main/java/com/coinhub/android/presentation/create_source/CreateSourceStates.kt
+++ app/src/main/java/com/coinhub/android/presentation/create_source/CreateSourceStates.kt
@@ -2,7 +2,7 @@ package com.coinhub.android.presentation.create_source
 
 sealed class CreateSourceStates {
     data class SourceCheckState(
-        val isValid: Boolean = true,
+        val isValid: Boolean = false,
         val errorMessage: String? = null,
     ) : CreateSourceStates()
 }
